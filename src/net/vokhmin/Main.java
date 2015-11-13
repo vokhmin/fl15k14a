fde51b489c6dd4193bb472f4abb721a760ab56b2
Andrey Vokhmin <andrey.vokhmin@outlook.com>
2015-11-14 01:49:44 +0300

    + test code to Main class;

--- src/net/vokhmin/Main.java
+++ src/net/vokhmin/Main.java
@@ -5,14 +5,33 @@ public class Main {
     public static void main(String[] args) {
 
         Circuit r12 = new Resistore(12.0);
-        Circuit r7 = new Resistore(7.0);
-        Circuit r5 = new Resistore(5.0);
+        Circuit r4 = new Resistore(4.0);
+        Circuit r2 = new Resistore(2.0);
+        double expected;
 
-        Circuit[] resistorsA = { r12, r12, r12 };
+        Circuit[] resistorsA = { r4, r4, r2 };
+
         Parallel parallelA = new Parallel(resistorsA);
-        assert parallelA.getResistance() == 12.0 / 3;
+        expected = 1 / ( 2 / 4.0 + 1 / 2.0 );
+        if (parallelA.getResistance() != expected)
+            System.out.printf("ERROR:1: expected - %f, but actual - %f", expected, parallelA.getResistance());
 
         Serial serialA = new Serial(resistorsA);
-        assert serialA.getResistance() == 12.0 * 3;
+        expected = 2 * 4.0 + 2.0;
+        if (serialA.getResistance() != expected)
+            System.out.printf("ERROR:2: expected - %f, but actual - %f", expected, serialA.getResistance());
+
+        Circuit circuit;
+
+        circuit = new Serial( new Circuit[] {parallelA, serialA} );
+        expected = 1.0 + 10.0;
+        if (circuit.getResistance() != expected)
+            System.out.printf("ERROR:3: expected - %f, but actual - %f", expected, circuit.getResistance());
+
+        circuit = new Parallel( new Circuit[] {parallelA, serialA} );
+        expected = 1 / ( 1 / 1.0 + 1 / 10.0);
+        if (circuit.getResistance() != expected)
+            System.out.printf("ERROR:4: expected - %f, but actual - %f", expected, circuit.getResistance());
+
     }
 }
